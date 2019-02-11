f8f91d1a43670bcaf52fc1d9e0ef82824925df76
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-10 21:50:06 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,28 +1,35 @@
-import InputModule
-import StorageModule
-import AiModule
-import AlertModule
-import UserInterfaceModule
+import InputModule_lxc
+from Storage import Storage
+from AiModule import AiModule
+import Alert_module
+import UserInterface_module
 
 
 def main():
-    path = '\'
-    time = []
-    value = []
-    time, value = InputModule.InputModule.read(path)
-    bo = value[0]
-    bp = value[1]
-    pul = value[2]
+    pathbo = './examplebo.txt'
+    pathbp = './examplebp.txt'
+    pathpul = './examplepul.txt'
+    bo = InputModule_lxc.input(pathbo)
+    bp = InputModule_lxc.input(pathbp)
+    pul = InputModule_lxc.input(pathpul)
+    Storage(bo, bp, pul)
+    s, j, k = Storage.read()
+    print(s, j, k)
 
-    storage = StorageModule.storage(bo, bp, pul)
-    storage.read()
 
-    AlertModule.Alert_for_three_categories_input(value)
-    AlertModule.Alert_Output()
-    ai = AiModule.AiModule()
+    ai = AiModule()
     ai.input_check(bo, bp, pul)
     predBloodOxygen, predBloodPressure, prePulse = ai.predict()
-    UserInterfaceModule.Userinterface_Input(predBloodOxygen, predBloodPressure, prePulse)
+    alt = Alert_module.Alert()
+    for k in range(len(bo)):
+        boi = bo[k], 0
+        bpi = bp[k], 1
+        puli = pul[k], 2
+        alt.Alert_for_three_categories_input(boi)
+        alt.Alert_for_three_categories_input(bpi)
+        alt.Alert_for_three_categories_input(puli)
+    UserInterface_module.userinterface_input(bo, bp, pul, predBloodOxygen, predBloodPressure, prePulse)
+    UserInterface_module.userinterface_output(bo, bp, pul, predBloodOxygen, predBloodPressure, prePulse)
 
 
 if __name__ == '__main__':
