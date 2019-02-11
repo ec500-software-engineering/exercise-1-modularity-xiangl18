4b9de724916a2b14942454c2fb5f0201df4f693b
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-10 21:47:37 -0500

    Create InputModule_lkn.py

---
+++ InputModule_lkn.py
@@ -0,0 +1,18 @@
+def read_data(path):
+    time = []
+    value = []
+    try:
+        with open(path,'r') as f:
+            data = f.readline()
+            if data:
+                for i in data:
+                    i = i.split()
+                    for item in i:
+                        value.append(float(item))
+                print("Read data successfully\n")
+                return value
+            else:
+                print("Empty data file!\n")
+                return 2
+    except:
+        print("Error:No input data\n")
