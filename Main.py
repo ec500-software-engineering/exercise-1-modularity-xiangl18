149116a40fedd974cb2aced39bc0bac29a2344d1
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2019-02-10 22:04:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,20 +6,24 @@ import UserInterface_module
 
 
 def main():
+#   Input Module   
     pathbo = './examplebo.txt'
     pathbp = './examplebp.txt'
     pathpul = './examplepul.txt'
     bo = InputModule_lxc.input(pathbo)
     bp = InputModule_lxc.input(pathbp)
     pul = InputModule_lxc.input(pathpul)
+    
+#   Input Module  
     Storage(bo, bp, pul)
-    s, j, k = Storage.read()
-    print(s, j, k)
-
-
+    print(Storage.read())
+    
+#   AI Module       
     ai = AiModule()
     ai.input_check(bo, bp, pul)
     predBloodOxygen, predBloodPressure, prePulse = ai.predict()
+    
+#   Alert Module, 0-blood oxygen, 1-blood pressure, 2-pulse
     alt = Alert_module.Alert()
     for k in range(len(bo)):
         boi = bo[k], 0
@@ -28,6 +32,8 @@ def main():
         alt.Alert_for_three_categories_input(boi)
         alt.Alert_for_three_categories_input(bpi)
         alt.Alert_for_three_categories_input(puli)
+        
+#   User Interface Module         
     UserInterface_module.userinterface_input(bo, bp, pul, predBloodOxygen, predBloodPressure, prePulse)
     UserInterface_module.userinterface_output(bo, bp, pul, predBloodOxygen, predBloodPressure, prePulse)
 
